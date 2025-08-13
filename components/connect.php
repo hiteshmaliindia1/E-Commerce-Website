7ab96a30d31ea58f7209b155e78bae35d781919a
Hitesh Mali <hiteshmaliindia@outlook.com>
2025-08-13 07:44:35 +0530

    Add files via upload

---
+++ components/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = 'mysql:host=sql312.infinityfree.com;dbname=epiz_34332643_ecommerce';
+$user_name = 'epiz_34332643';
+$user_password = 'uPOGLgUJvY';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="admin/dashboard.php"> <i class="fas fa-angle-right"></i> Admin</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:9499781762"><i class="fas fa-phone"></i> +91 949 978 1762</a>
+         <a href="tel:9499781762"><i class="fas fa-phone"></i> +91 949 978 1762</a>
+         <a href="mailto:hiteshmaliindia@gmail.com"><i class="fas fa-envelope"></i> hiteshmaliindia@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Gujarat, India </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/hiteshmaliindia" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://x.com/hiteshmaliindia" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="https://www.instagram.com/hiteshmaliindia/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="https://www.linkedin.com/in/hiteshmaliindia" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; E-Commerce <?= date('Y'); ?> by <span>Hitesh Mali</span> | All Rights Reserved!</div>
+
+</footer>
